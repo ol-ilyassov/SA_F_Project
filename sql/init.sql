fc113e3ba784b961e79233dee0b8dae4b4319d02
Olzhas <ol.ilyassov@gmail.com>
2021-03-10 00:55:44 +0600

    Docker Step 1

---
+++ sql/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE articles(
+  id serial not null unique, 
+  authorid int not null,
+  title varchar(100) not null, 
+  content text not null, 
+  created timestamp not null, 
+  expires timestamp not null
+);
+
+ALTER TABLE articles ADD CONSTRAINT fk_authorid 
+FOREIGN KEY (authorid) REFERENCES users (id);
+
+CREATE INDEX idx_articles_created ON snippets(created);
+
+CREATE TABLE users (
+  id SERIAL NOT NULL UNIQUE,
+  authorid INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  hashed_password CHAR(60) NOT NULL,
+  created timestamp NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
